62f7f1251780a2b79956fa1999c83e9ed03b576e
Matthieu Aussaguel <matthieu,aussaguel@gmail.com>
2011-09-25 15:22:24 +1000

    Do some mad refactoring

--- mad_libs/lib/mad_lib.rb
+++ mad_libs/lib/mad_lib.rb
@@ -2,35 +2,45 @@ class MadLib
 
 	def initialize filename
 		@filepath = File.join(APP_ROOT, 'patterns', filename)
-		
+		@placeholder_map = {}
+		@text = ""
 	end
 
 	def launch!
-		print_welcome_message
-		unless !File.exists? @filepath
-			placeholders.each do |placeholder|
-				puts placeholder
-			end
+		print welcome_message
+		unless !File.exists? @filepath	
+			fetch_text
 
+			map_placeholder			
 		end
 	end
 
 	private
-	def print_welcome_message
-		puts "\n" 
-		puts "*************** Welcome to Mad Lib ***************"
-		puts "\n\n"
+	def welcome_message
+		"\n*************** Welcome to Mad Lib ***************\n\n"
+	end
+
+	def fetch_text
+		File.open(@filepath, "r") do |file|
+			while (line = file.gets)
+				@text += line			
+	  		end
+		end
 	end
 
 	def placeholders
-		unless @placeholders
-		@placeholders = []
-		File.open(@filepath, "r") do |file|
-			while (line = file.gets)
-  				@placeholders += line.scan(/\(\(([^(\)\))]+)\)\)/).flatten   				
-  			end
-			end
+ 		@text.scan(placeholder_pattern).flatten   				
+	end
+
+	def placeholder_pattern
+		/\(\(([^(\)\))]+)\)\)/
+	end
+
+	def map_placeholder
+		placeholders.each do |placeholder|
+			print "Give me #{placeholder}: "
+			@placeholder_map[placeholder] = gets.chomp.strip
 		end
-		@placeholders
+		# @placeholder_map = "{\"a verb, past tense\"=>\"sad\", \"a number\"=>\"2\", \"a plural noun\"=>\"dsf\", \"a noun\"=>\"asd\", \"a verb ending in ing\"=>\"sad\", \"an adjective\"=>\"dfds\", \"an adverb\"=>\"ad\", \"a body part\"=>\"sgd\"}"
 	end
 end
