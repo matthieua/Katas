f69d03c24abdd12d385b36fffae053f9c8ec223a
Matthieu Aussaguel <matthieu,aussaguel@gmail.com>
2011-09-25 14:06:25 +1000

    Add MadLib application folder and Hello World

---
+++ mad_libs/init.rb
@@ -0,0 +1,17 @@
+#### Mad Libs ####
+#
+# Launch this Ruby file from the command line 
+# to get started.
+#
+
+APP_ROOT = File.dirname(__FILE__)
+
+# require "#{APP_ROOT}/lib/guide"
+# require File.join(APP_ROOT, 'lib', 'guide.rb')
+# require File.join(APP_ROOT, 'lib', 'guide')
+
+$:.unshift( File.join(APP_ROOT, 'lib') )
+require 'mad_lib'
+
+app = MadLib.new
+app.launch!
---
+++ mad_libs/lib/mad_lib.rb
@@ -0,0 +1,14 @@
+class MadLib
+
+	def launch!
+		print_welcome_message
+	end
+
+	private
+	def print_welcome_message
+		puts "\n" 
+		puts "*************** Welcome to Mad Lib ***************"
+		puts "\n\n"
+	end
+
+end
